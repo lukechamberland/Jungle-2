b8c9437747e3a0dc9f38b3f00365b8c062808b76
Luke Chamberland <luke.chamberland@shaw.ca>
2023-07-16 12:45:47 +0000

    add spec files

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('Home Page', () => {
+  it('Visits the home page', () => {
+    cy.visit('/');
+  });
+  
+  it('There are products on the page', () => {
+    cy.get('.products article').should('be.visible');
+  });
+
+  it('There are 2 products on the page', () => {
+    cy.get('.products article').should('have.length', 2);
+  });
+
+});
